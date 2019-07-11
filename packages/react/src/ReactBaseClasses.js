46b838a6676f1f024b080887ee8215fe9a75ae56
i am a developer <shamloo.1370@gmail.com>
2019-07-11 21:09:19 +0430

    init

--- packages/react/src/ReactBaseClasses.js
+++ packages/react/src/ReactBaseClasses.js
@@ -19,6 +19,7 @@ if (__DEV__) {
  * Base class helpers for the updating state of a component.
  */
 function Component(props, context, updater) {
+  console.log('Component called')
   this.props = props;
   this.context = context;
   // If a component has string refs, we will assign a different object later.
